844e0732fb6520cfa2d5af6048873d5c8e5b0513
Kyle Spresser <kylespresser@gmail.com>
2024-03-06 23:37:17 -0500

    completed magic_trick exercise

--- magic_trick_starter.py
+++ magic_trick_starter.py
@@ -8,25 +8,26 @@
 #                |___/        
 
 # Create a variable called secret_num and set it to any number you'd like:
-
+original_num = 69
+secret_num = original_num
 
 # Increase secret_num by 1
-
+secret_num += 1
 
 # Next, take the new number and double it
-
+secret_num *= 2
 
 # Next, take the new number and add four to it
-
+secret_num += 4
 
 # Next, take the new number and divide by two
-
+secret_num /= 2
 
 # Next, take the new number and subtract your original secret number from it
-
+secret_num = secret_num - original_num
 
 # Print the final result
-
+print(secret_num)
 
 
 # To see my magical prediction, scroll down...
